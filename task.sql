d597454be33ff380238b1540a1d234ac94b04011
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2025-08-07 22:14:53 +0300

    fix2

--- task.sql
+++ task.sql
@@ -29,8 +29,8 @@ CREATE TABLE ProductDescription (
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
     ID INT,
-    Time DATETIME,
-    LogRecord VARCHAR(255),
+    Timestamp DATETIME,
+    Message VARCHAR(255),
     PRIMARY KEY (ID)
 ) ENGINE=BLACKHOLE;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
